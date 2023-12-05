caed80bab539c17f11ab12d93a9ad44d700ff6a4
vaclavbicha <144042610+vaclavbicha@users.noreply.github.com>
2023-12-05 15:39:14 +0000

    New_menu

---
+++ Assets/Mainmeni.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuV2 : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/SceneLoading.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoading : MonoBehaviour
+{
+    [SerializeField]
+    private Image _progressBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //start async operation
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+    IEnumerator LoadAsyncOperation()
+    {
+        //create an async operation
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(2);
+        
+        while (gameLevel.progress < 1)
+        {
+            _progressBar.fillAmount = gameLevel.progress;
+            yield return new WaitForEndOfFrame();
+        }
+        
+    }
+}
